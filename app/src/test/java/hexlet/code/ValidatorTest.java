3839e11f47e6b2190992281381b648754feb982f
Alex <alextetekin@gmail.com>
2023-08-15 11:44:12 +0300

    test: created tests

--- app/src/test/java/hexlet/code/ValidatorTest.java
+++ app/src/test/java/hexlet/code/ValidatorTest.java
@@ -1,7 +1,30 @@
 package hexlet.code;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 
 class ValidatorTest {
+    private final Validator validator = new Validator();
 
+    @ParameterizedTest
+    @CsvSource({
+            "test with all correct requirement, test, 5, true",
+            "test with wrong length, test, 50, false",
+            "test with wrong contains, alex, 7, false",
+    })
+    void validatorTestWithRequirement(String text, String requirement, int length, boolean expectedResult) {
+
+        boolean actual = validator.string().contains(requirement).minLength(length).isValid(text);
+        Assertions.assertEquals(expectedResult, actual);
+    }
+
+    @Test
+    void validatorTestWithNull() {
+        String text = "";
+        boolean actual = new StringSchema().required().isValid(text);
+        Assertions.assertNotNull(actual);
+    }
 }
