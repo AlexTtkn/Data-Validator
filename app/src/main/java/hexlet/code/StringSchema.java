82347e170e23da3d5ce4b66201d8a14a630ae33e
Alex <alextetekin@gmail.com>
2023-08-21 10:41:45 +0300

    build: changed StringSchema.java

--- app/src/main/java/hexlet/code/StringSchema.java
+++ app/src/main/java/hexlet/code/StringSchema.java
@@ -1,48 +1,20 @@
 package hexlet.code;
 
-public class StringSchema {
-    private boolean required;
-    private int minLength;
-    private String contains;
-
+public class StringSchema extends BaseSchema {
 
     public StringSchema required() {
-        required = true;
+        addError("required", value -> (value instanceof String) && (!value.equals("")));
         return this;
     }
 
     public StringSchema minLength(int length) {
-        minLength = length;
+        addError("minLength", value -> (value == null) || (value.toString().length() >= length));
         return this;
     }
 
     public StringSchema contains(String substring) {
-        contains = substring;
+        addError("contains", value -> (value != null) && (value.toString().contains(substring)));
         return this;
     }
 
-    public boolean isValid(String value) {
-        if (isRequiredInvalid(value)) {
-            return false;
-        }
-        if (isLengthInvalid(value)) {
-            return false;
-        }
-        if (isContainsInvalid(value)) {
-            return false;
-        }
-        return true;
-    }
-
-    private boolean isRequiredInvalid(String value) {
-        return required && (value == null || value.isEmpty());
-    }
-
-    private boolean isLengthInvalid(String value) {
-        return value != null && value.length() < minLength;
-    }
-
-    private boolean isContainsInvalid(String value) {
-        return contains != null && !value.contains(contains);
-    }
 }
